0c1c9d7fae6faa67cfc2333f7237f177c460bb61
storkych <79412391+storkych@users.noreply.github.com>
2023-10-21 04:03:12 +0300

    Пытаюсь, пока не оч

--- SnakeGame/SnakeGame/Program.cs
+++ SnakeGame/SnakeGame/Program.cs
@@ -7,11 +7,9 @@ class Program
 {
     static void Main()
     {
-        ConsoleKeyInfo keyInfo;
-        int selectedItem = 0;
-        bool isPlaying = false;
-
         Console.CursorVisible = false;
+        Console.BackgroundColor = ConsoleColor.Black;
+        bool isPlaying = false;
 
         while (true)
         {
@@ -19,49 +17,79 @@ class Program
 
             if (isPlaying)
             {
-                SnakeGame snakeGame = new SnakeGame(Console.WindowWidth, Console.WindowHeight - 1);
-                snakeGame.PlayGame();
-                isPlaying = false;
+                // Игровое поле
+
+                ConsoleKeyInfo keyInfo = Console.ReadKey(true);
+
+                if (keyInfo.Key == ConsoleKey.Escape)
+                {
+                    isPlaying = false;
+                }
             }
             else
             {
-                string[] menuItems = { "Продолжить игру", "Новая игра", "Таблица рекордов", "Выход" };
+                string[] menuItems = { "1. Начать игру", "2. Выйти" };
+                int selectedItem = 0;
 
-                for (int i = 0; i < menuItems.Length; i++)
+                while (true)
                 {
-                    if (i == selectedItem)
+                    Console.Clear();
+
+                    for (int i = 0; i < menuItems.Length; i++)
                     {
-                        Console.ForegroundColor = ConsoleColor.White;
-                    }
-                    else
-                    {
-                        Console.ForegroundColor = ConsoleColor.Gray;
+                        Console.ForegroundColor = i == selectedItem ? ConsoleColor.White : ConsoleColor.Gray;
+                        Console.WriteLine(i == selectedItem ? ">> " + menuItems[i] : "   " + menuItems[i]);
                     }
 
-                    Console.WriteLine((i == selectedItem ? ">> " : "   ") + menuItems[i]);
-                }
+                    ConsoleKeyInfo keyInfo = Console.ReadKey(true);
 
-                keyInfo = Console.ReadKey(true);
+                    if (keyInfo.Key == ConsoleKey.W && selectedItem > 0)
+                    {
+                        selectedItem--;
+                    }
+                    else if (keyInfo.Key == ConsoleKey.S && selectedItem < menuItems.Length - 1)
+                    {
+                        selectedItem++;
+                    }
+                    else if (keyInfo.Key == ConsoleKey.Enter)
+                    {
+                        if (selectedItem == 0)
+                        {
+                            StartGame();
+                            isPlaying = true;
+                        }
+                        else
+                        {
+                            return;
+                        }
+                    }
+                }
+            }
+        }
+    }
 
-                if (keyInfo.Key == ConsoleKey.W && selectedItem > 0)
-                {
-                    selectedItem--;
-                }
-                else if (keyInfo.Key == ConsoleKey.S && selectedItem < menuItems.Length - 1)
-                {
-                    selectedItem++;
-                }
-                else if (keyInfo.Key == ConsoleKey.Enter)
-                {
-                    if (selectedItem == 3)
-                    {
-                        break;
-                    }
-                    else if (selectedItem == 1)
-                    {
-                        isPlaying = true;
-                    }
-                }
+    static void StartGame()
+    {
+        int width = 80;
+        int height = 40;
+
+        SnakeGame game = new SnakeGame(width, height);
+        game.Run();
+
+        Console.Clear();
+        Console.WriteLine("  ▄▄ •  ▄▄▄· • ▌ ▄ ·. ▄▄▄ .         ▌ ▐·▄▄▄ .▄▄▄  ");
+        Console.WriteLine(" ▐█ ▀ ▪▐█ ▀█ ·██ ▐███▪▀▄.▀·   ▄█▀▄ ▪█·█▌▀▄.▀·▀▄ █·");
+        Console.WriteLine(" ▄█ ▀█▄▄█▀▀█ ▐█ ▌▐▌▐█·▐▀▀▪▄  ▐█▌.▐▌▐█▐█•▐▀▀▪▄▐▀▀▄ ");
+        Console.WriteLine(" ▐█▄▪▐█▐█▪ ▐▌██ ██▌▐█▌▐█▄▄▌  ▐█▌.▐▌ ███ ▐█▄▄▌▐█•█▌");
+        Console.WriteLine("  ·▀▀▀▀  ▀  ▀ ▀▀  █▪▀▀▀ ▀▀▀    ▀█▄▀▪. ▀   ▀▀▀ .▀  ▀");
+        Console.WriteLine("\nИгра завершена. Ваш счет: " + game.Score);
+
+        while (true)
+        {
+            ConsoleKeyInfo keyInfo = Console.ReadKey(true);
+            if (keyInfo.Key == ConsoleKey.Enter)
+            {
+                return;
             }
         }
     }
@@ -69,65 +97,64 @@ class Program
 
 class SnakeGame
 {
-    private int screenWidth;
-    private int screenHeight;
+    private int width;
+    private int height;
     private int snakeX;
     private int snakeY;
     private int foodX;
     private int foodY;
-    private int score;
     private List<int> tailX;
     private List<int> tailY;
+    private int score;
     private bool isGameOver;
     private Direction direction;
 
+    public int Score { get; private set; }
+
     public SnakeGame(int width, int height)
     {
-        screenWidth = width;
-        screenHeight = height;
+        this.width = width;
+        this.height = height;
         snakeX = width / 2;
         snakeY = height / 2;
-        foodX = new Random().Next(1, width - 2);
-        foodY = new Random().Next(1, height - 2);
-        score = 0;
         tailX = new List<int>();
         tailY = new List<int>();
+        foodX = new Random().Next(1, width - 1);
+        foodY = new Random().Next(1, height - 1);
+        score = 0;
         isGameOver = false;
         direction = Direction.Right;
+        Console.CursorVisible = false;
     }
 
-    public void PlayGame()
+    public void Run()
     {
-        ConsoleKeyInfo keyInfo;
         while (!isGameOver)
         {
             if (Console.KeyAvailable)
             {
-                keyInfo = Console.ReadKey(true);
-                ChangeDirection(keyInfo.Key);
+                var key = Console.ReadKey(true).Key;
+                ProcessKey(key);
             }
 
             MoveSnake();
             if (snakeX == foodX && snakeY == foodY)
             {
                 EatFood();
-                foodX = new Random().Next(1, screenWidth - 2);
-                foodY = new Random().Next(1, screenHeight - 2);
+                GenerateFood();
             }
 
-            if (snakeX == 0 || snakeX == screenWidth - 1 || snakeY == 0 || snakeY == screenHeight - 1 || CheckCollisionWithTail())
+            if (snakeX == 0 || snakeX == width - 1 || snakeY == 0 || snakeY == height - 1 || CheckCollisionWithTail())
             {
                 isGameOver = true;
             }
 
-            DrawGameArea();
-            DrawSnake();
-            DrawFood();
+            DrawGame();
             Thread.Sleep(100);
         }
     }
 
-    private void ChangeDirection(ConsoleKey key)
+    private void ProcessKey(ConsoleKey key)
     {
         switch (key)
         {
@@ -150,20 +177,21 @@ class SnakeGame
         }
     }
 
-
     private void MoveSnake()
     {
         int prevX = snakeX;
         int prevY = snakeY;
-        if (tailX.Count > 0)
+
+        for (int i = 0; i < tailX.Count; i++)
         {
-            prevX = tailX.Last();
-            prevY = tailY.Last();
-            tailX.Insert(0, prevX);
-            tailY.Insert(0, prevY);
-            tailX.RemoveAt(tailX.Count - 1);
-            tailY.RemoveAt(tailY.Count - 1);
+            int tempX = tailX[i];
+            int tempY = tailY[i];
+            tailX[i] = prevX;
+            tailY[i] = prevY;
+            prevX = tempX;
+            prevY = tempY;
         }
+
         switch (direction)
         {
             case Direction.Up:
@@ -181,6 +209,19 @@ class SnakeGame
         }
     }
 
+    private void EatFood()
+    {
+        score++;
+        tailX.Add(0);
+        tailY.Add(0);
+    }
+
+    private void GenerateFood()
+    {
+        foodX = new Random().Next(1, width - 1);
+        foodY = new Random().Next(1, height - 1);
+    }
+
     private bool CheckCollisionWithTail()
     {
         for (int i = 0; i < tailX.Count; i++)
@@ -191,51 +232,22 @@ class SnakeGame
         return false;
     }
 
-    private void EatFood()
+    private void DrawGame()
     {
-        score++;
-        tailX.Add(tailX.Last());
-        tailY.Add(tailY.Last());
-    }
+        Console.SetCursorPosition(foodX, foodY);
+        Console.Write("■");
 
-    private void DrawGameArea()
-    {
-        Console.BackgroundColor = ConsoleColor.Black;
-        Console.ForegroundColor = ConsoleColor.Red;
-
-        for (int i = 0; i < screenWidth; i++)
-        {
-            Console.SetCursorPosition(i, 0);
-            Console.Write("█");
-            Console.SetCursorPosition(i, screenHeight - 1);
-            Console.Write("█");
-        }
-        for (int i = 0; i < screenHeight; i++)
-        {
-            Console.SetCursorPosition(0, i);
-            Console.Write("█");
-            Console.SetCursorPosition(screenWidth - 1, i);
-            Console.Write("█");
-        }
-    }
-
-    private void DrawSnake()
-    {
         Console.SetCursorPosition(snakeX, snakeY);
         Console.Write("■");
+
         for (int i = 0; i < tailX.Count; i++)
         {
             Console.SetCursorPosition(tailX[i], tailY[i]);
             Console.Write("■");
         }
-    }
 
-    private void DrawFood()
-    {
-        Console.SetCursorPosition(foodX, foodY);
-        Console.ForegroundColor = ConsoleColor.Green;
-        Console.Write("■");
-        Console.ForegroundColor = ConsoleColor.Red;
+        Console.SetCursorPosition(0, height);
+        Console.Write($"Score: {score}");
     }
 }
 
